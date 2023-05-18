204d8e4864926c28c685eee5382780678b571584
m-asaoka-fap <66696898+m-asaoka-fap@users.noreply.github.com>
2023-05-19 08:35:40 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,7 @@
-input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-    custom.SetAvatar(Avatar.Happy)
-    custom.SetTextAction(CustomTextAction.SayHappy)
-    basic.showIcon(IconNames.Happy)
-})
-input.onButtonPressed(Button.A, function () {
-    custom.SetPanAction(CustomPanAction.PanRight)
-})
-input.onGesture(Gesture.Shake, function () {
-    custom.SetAvatar(Avatar.Angry)
-    custom.SetTextAction(CustomTextAction.SayAngry)
-    basic.showIcon(IconNames.Angry)
-})
-input.onButtonPressed(Button.AB, function () {
-    custom.SetTiltAction(CustomTiltAction.TiltFlont)
-    custom.SetPanAction(CustomPanAction.PanFlont)
-    custom.SetAvatar(Avatar.Neutral)
-    custom.SetTextAction(CustomTextAction.Off)
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        . # # # .
-        . . . . .
-        `)
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    custom.TiltY(30)
+    custom.TiltY(-30)
+    custom.TiltY(-30)
 })
 custom.start()
 basic.showLeds(`
